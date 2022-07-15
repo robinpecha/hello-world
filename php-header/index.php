6eb2a478bac1aa8ea6f965f36ab6035d0c59cf61
robinpecha <robinpecha@gmail.com>
2022-07-15 13:36:30 +0200

    Update index.php

--- php-header/index.php
+++ php-header/index.php
@@ -1,4 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ShowAllHeaders</title>
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+</head>
+<body>
+    
 <h1>get_headers()</h1>
 <?php
 $URL = 'http://zoopla.robinpecha.cz/';
@@ -44,3 +54,6 @@ foreach ($apache_headers as $key => $value) {
     foreach($headers as $key=>$val){
     echo $key . ' => ' . $val . ' <br> ';}
 ?>
+
+</body>
+</html>

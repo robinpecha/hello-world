89dc52a3df0b56fa88007bf2cd315f11eac3c913
robinpecha <robinpecha@gmail.com>
2023-03-02 12:00:48 +0100

    dd

--- git.sh
+++ git.sh
@@ -1,9 +1,10 @@
 git fetch --all
 git pull --all
+
 git fetch
 git branch -r #list branches
 
+#delete branch
 git fetch -p #The -p flag means "prune". After fetching, branches which no longer exist on the remote will be deleted.
-git branch -d disabled # delete branch locally
-git branch -d disabled # delete branch remotely
-git push origin --delete disabled
+git branch -d localBranchName # delete branch locally
+git push origin --delete remoteBranchName # delete branch remotely
